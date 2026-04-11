fc6b6a66734996b71cfe0a74bbf3399dc591d192
Paras Surti <paras16surti@gmail.com>
2026-04-11 12:00:51 +0530

    Printing an error message with user input - {command}: command not found

--- src/shell/Main.java
+++ src/shell/Main.java
@@ -1,9 +1,20 @@
 package shell;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
         System.out.print("$ ");
+
+        //User Input...
+        Scanner input = new Scanner(System.in);
+        String name = input.next();
+
+        //Output for user...
+        System.out.println(name + ": command not found ");
+
     }
 }
