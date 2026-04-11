6e43c306f7e0ddb82004d83e642fb65b6efb3d1d
Paras Surti <paras16surti@gmail.com>
2026-04-11 14:38:18 +0530

    Implementation of echo built-in inside REPL.

--- src/shell/Main.java
+++ src/shell/Main.java
@@ -6,20 +6,38 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.print("$ ");
-
-        //User Input...
         Scanner input = new Scanner(System.in);
+        REPL(input);
 
-        //Exit command inside REPL
+    }
+
+    public static void REPL(Scanner input)
+    {
+        //implementation the echo builtin inside REPL.
         while(true)
         {
+            System.out.print("$ ");
             String name = input.nextLine();
+
+            //would exit the REPL
             if(name.equals("exit"))
             {
                 break;
             }
-            System.out.println(name + " : command not found");
+
+            //Used of echo build-in
+            else if(name.contains("echo "))
+            {
+                System.out.println(name.substring(5));
+
+            }
+
+            //if command is not found & would be inside the REPL loop...
+            else
+            {
+                System.out.println(name + " : command not found");
+            }
+
         }
     }
 }
