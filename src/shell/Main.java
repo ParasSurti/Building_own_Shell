f2b31dedc26ab5d3ef02e27ce700e3ea5f273590
Paras Surti <paras16surti@gmail.com>
2026-04-14 12:25:47 +0530

    Implemented type command with PATH search.

--- src/shell/Main.java
+++ src/shell/Main.java
@@ -1,5 +1,7 @@
 package shell;
 
+import java.io.File;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Main
@@ -8,7 +10,6 @@ public class Main
     {
         Scanner input = new Scanner(System.in);
         REPL(input);
-
     }
 
     public static void REPL(Scanner input)
@@ -25,22 +26,53 @@ public class Main
                 break;
             }
 
-            //Used of echo build-in
+            //Used of echo built-in
             else if(name.startsWith("echo"))
             {
                 System.out.println(name.substring(5));
             }
 
+            //Used of type built-in
             else if(name.startsWith("type "))
             {
-                if(name.substring(5).equals("echo") || name.substring(5).equals("exit"))
+                //get command after "type "
+                String command = name.substring(5);
+
+                if(command.equals("echo") || command.equals("exit") || command.equals("type"))
                 {
-                    System.out.println( name.substring(5) + " is a shell builtin");
+                    System.out.println(command + " is a shell builtin");
                 }
+
+                //Step 2 : search Path
                 else
                 {
-                    System.out.println(name.substring(5) + " : not found ");
+                    String path = System.getenv("PATH");
+                    String[] directories = path.split(":");
+                    boolean found = false;
+
+                    //step 3: Loop through folders
+                    for(String dir : directories)
+                    {
+                        //Step 4 : Build full path & check file
+                        File file = new File(dir,command);
+
+                        //Step 5 : exits AND executable ?
+                        if(file.exists() && file.canExecute())
+                        {
+                            System.out.println(command + " is " + file.getAbsolutePath());
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    //Step 6: After loop
+                    if(!found)
+                    {
+                        System.out.println(command + ": not found");
+                    }
                 }
+
+
             }
 
             //if command is not found & would be inside the REPL loop...
@@ -48,7 +80,6 @@ public class Main
             {
                 System.out.println(name + " : command not found");
             }
-
         }
     }
 }
